603c851a1d655add137f2ebb01e1ddc6acff97ca
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-10-17 11:49:25 +0400

    init commit

---
+++ api/init.py
@@ -0,0 +1,17 @@
+from email import message
+from flask import Flask, request, render_template
+from flask_cors import CORS
+from flask_mail import Mail, Message
+import os, requests, json
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def landing():
+    data = request.get_json()
+    return {'message': 'Hello, World! This is a private API used by the DailyShow WhatsappBot.'}, 200
