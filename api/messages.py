2889d827b3dc872135c1fd19b21f611e9b3e33da
snarang181 <70980689+snarang181@users.noreply.github.com>
2022-10-17 15:58:35 +0400

    echo back messages

--- api/messages.py
+++ api/messages.py
@@ -1,7 +1,12 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+meta_auth_token = 'Bearer ' + os.environ.get('meta_api_token')
 def send_message(phone_num, phone_num_id, message):
+    
     global meta_auth_token    
     base_url = 'https://graph.facebook.com/v13.0/' + str(phone_num_id) + '/messages'
     headers = {'Authorization' : meta_auth_token, 'Content-Type': 'application/json'}
@@ -14,7 +19,7 @@ def send_message(phone_num, phone_num_id, message):
     r = requests.post(base_url, data=json.dumps(body), headers=headers)
     print(r.json())
 
-# def command_message(): 
+
     
     
     
